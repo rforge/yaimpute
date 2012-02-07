92edd0a05d738f77d1761a36ce338d4d786fa3d3
ncrookston <ncrookston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-02-07 01:31:15 +0000

    Added use of "quantile" to notablydistant and worked on correctbias (not yet ready for general use).
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yaimpute@9 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yaImpute/R/correctbias.R
+++ pkg/yaImpute/R/correctbias.R
@@ -1,68 +1,98 @@
-correctBias = function (object,trgVal,trgValCI=NULL,nStdev=1.5,trace=FALSE)
+correctBias = function (object,trgVal,trgValCI=NULL,nStdev=1.5,excludeRefIds=NULL,trace=FALSE)
 {
   if (class(object) != "yai") stop("object must be of class yai")
-  if (object$k < 2) stop("k in object must be greater than 1")
-  if (is.null(object$neiIdsTrgs)) stop("No targets, nothing to correct.")
 
   if (missing(trgVal)) stop("trgVal must be defined.")
   trgValExpression = if (is.expression(trgVal)) trgVal else
     if (class(trgVal) == "character") parse(text=trgVal) else NULL
-  if (!is.expression(trgValExpression)) stop ("trgVal can be coerced into an expression.")
+  if (!is.expression(trgValExpression)) stop ("trgVal can not be coerced into an expression.")
 
-  trgValData <- as.data.frame(cbind(object$yRefs,object$xRefs)) 
-  trgVals <- eval(trgValExpression,trgValData)
-  trgVal <- mean(trgVals)
-  trgValsd <- sd(trgVals)/sqrt(nrow(trgValData))
-  trgValCIc <- c(trgVal-(trgValsd*nStdev),trgVal+(trgValsd*nStdev))
+  trgVals <- NULL
+  trgVal <-  NULL
+  trgValsd <- NULL
+  trgValCIc <- NULL
+  excludeRefIds <- if (is.null(excludeRefIds)) "none" else excludeRefIds
+  if (excludeRefIds == "all")
+  {
+    refsToCount <- NULL      
+  }
+  else
+  {
+    refsToCount <- 1:nrow(object$yRefs)
+    trgValData <- as.data.frame(cbind(object$yRefs,object$xRefs))
+    trgValDataCopy <- trgValData
+    if (class(excludeRefIds) == "character") remove <- 
+      na.omit(match(excludeRefIds,rownames(trgValDataCopy)))
+    if (length(remove) > 0) 
+    {
+      trgValDataCopy <- trgValDataCopy[-remove,]
+      refsToCount <- refsToCount[-remove]
+      if (length(refsToCount) == 0) refsToCount <- NULL 
+    }
+    trgVals <- eval(trgValExpression,trgValDataCopy)
+    trgVal <- mean(trgVals)
+    trgValsd <- sd(trgVals)/sqrt(nrow(trgValDataCopy))
+    trgValCIc <- c(trgVal-(trgValsd*nStdev),trgVal+(trgValsd*nStdev))
+  }
   
   # if the user specified the CI, use it instead.
   if (is.null(trgValCI)) trgValCI <- trgValCIc
-  
+  if (is.null(trgValCI)) stop ("trgValIC was not supplied and can not be computed given the data.")
+    
   if (trace) cat ("Target CI=", trgValCI,"\n");flush.console()
   
   newobject <- object
-  for (pass in 1:(object$k-1))
+  pass = 0
+  if (object$k-1 > 0) 
   {
-    curVals <- eval(trgValExpression,trgValData[newobject$neiIdsTrgs[,1],,drop=FALSE])
-    curVal <- mean(curVals)
-    if (curVal >= trgValCI[1] & curVal <= trgValCI[2]) 
+    for (pass in 1:(object$k-1))
     {
-      if (trace) cat ("trgValCI=",trgValCI," curVal=",curVal)
-      if (pass == 1)
+      curVals <- eval(trgValExpression,trgValData[newobject$neiIdsTrgs[,1],,drop=FALSE])
+      curVal <- if (is.null(trgVals)) mean(curVals) else mean(c(curVals,trgVals))
+      if (curVal >= trgValCI[1] & curVal <= trgValCI[2]) 
       {
-        pass = 0
-        if (trace) cat (" -- no bias to correct.\n"); flush.console()
+        if (trace) cat ("trgValCI=",trgValCI," curVal=",curVal)
+        if (pass == 1)
+        {
+          pass = 0
+          if (trace) cat (" -- no bias to correct.\n"); flush.console()
+        }
+        else
+        {
+          pass=pass-1
+          if (trace) cat (" -- target CI reached, passes made=",pass,"\n");flush.console()
+        }
+        break
       }
-      else
-      {
-        pass=pass-1
-        if (trace) cat (" -- target CI reached, passes made=",pass,"\n");flush.console()
-      }
-      break
-    }
-
-    toHigh <- curVal > trgValCI[2]
-    curBias <- if (toHigh) curVal - trgValCI[2] else curVal - trgValCI[1]
-    # dV is the contribution to the bias of each observation.
-    dV <- (eval(trgValExpression,trgValData[newobject$neiIdsTrgs[,pass+1],,drop=FALSE]) - curVals) / length(curVals)
-    dDst <- newobject$neiDstTrgs[,pass+1] - newobject$neiDstTrgs[,1]
-    if (toHigh) {
-      ntoTry <- sum(dV < 0)
-      dDst[dV > 0] <- .Machine$double.xmax 
-    } else {
-      ntoTry <- sum(dV > 0)
-      dDst[dV < 0] <- .Machine$double.xmax
-    }
-    ord <- order(dDst)
-    bsum <- cumsum(dV[ord[1:ntoTry]])
-    toSwitch <- which(bsum+curBias > 0)[1]
-    if (is.na(toSwitch)) toSwitch <- length(bsum)
-
-    if (trace) cat ("trgValCI=",trgValCI," pass=",pass," curVal=",curVal," curBias=",curBias,
-       " ntoTry=",ntoTry,"toSwitch=",toSwitch,"\n");flush.console()
     
-    newobject$neiDstTrgs[ord[1:toSwitch],1] <- newobject$neiDstTrgs[ord[1:toSwitch],pass+1]
-    newobject$neiIdsTrgs[ord[1:toSwitch],1] <- newobject$neiIdsTrgs[ord[1:toSwitch],pass+1]
+      toHigh <- curVal > trgValCI[2]
+      curBias <- if (toHigh) curVal - trgValCI[2] else curVal - trgValCI[1]
+      # dV is the contribution to the bias of each observation.
+      dV <- (eval(trgValExpression,trgValData[newobject$neiIdsTrgs[,pass+1],,drop=FALSE]) - curVals) / length(curVals)
+      dDst <- newobject$neiDstTrgs[,pass+1] - newobject$neiDstTrgs[,1]
+      if (toHigh) {
+        ntoTry <- sum(dV < 0)
+        dDst[dV > 0] <- .Machine$double.xmax 
+      } else {
+        ntoTry <- sum(dV > 0)
+        dDst[dV < 0] <- .Machine$double.xmax
+      }
+      ord <- order(dDst)
+      bsum <- cumsum(dV[ord[1:ntoTry]])
+      toSwitch <- which(bsum+curBias > 0)[1]
+      if (is.na(toSwitch)) toSwitch <- length(bsum)
+    
+      if (trace) cat ("trgValCI=",trgValCI," pass=",pass," curVal=",curVal," curBias=",curBias,
+         " ntoTry=",ntoTry,"toSwitch=",toSwitch,"\n");flush.console()
+      
+      newobject$neiDstTrgs[ord[1:toSwitch],1] <- newobject$neiDstTrgs[ord[1:toSwitch],pass+1]
+      newobject$neiIdsTrgs[ord[1:toSwitch],1] <- newobject$neiIdsTrgs[ord[1:toSwitch],pass+1]
+    }
+  }
+  else
+  {
+    pass = 0
+    curVal = trgVal
   }
  
   # get rid of the k>1 columns...
