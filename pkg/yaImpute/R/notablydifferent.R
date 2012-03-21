5b22ea99e4fe693b55c9fb50e1fc50f3a381d169
ncrookston <ncrookston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-03-21 16:49:57 +0000

    Fixed error in computing scaled RMSD and set the default "vars" to the X variables in object.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yaimpute@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yaImpute/R/notablydifferent.R
+++ pkg/yaImpute/R/notablydifferent.R
@@ -4,7 +4,9 @@ notablyDifferent <- function (object,vars=NULL,threshold=NULL,p=.05,...)
    if (class(object)[1] != "yai")  stop ("object must be class yai")
    refIds <- rownames(object$neiDstRefs)
    if (length(refIds) == 0) stop ("references are required")
+   cl <- match.call()
    trgIds <- rownames(object$neiDstTrgs)
+   if (is.null(vars)) vars=xvars(object)
    impObj <- impute.yai(object,vars=vars,observed=TRUE,...)
    if (is.null(impObj)) stop ("no imputations found using this object")
    nuke <- unlist(lapply(impObj,function (x) all(is.na(x))))
@@ -31,13 +33,16 @@ notablyDifferent <- function (object,vars=NULL,threshold=NULL,p=.05,...)
    both <- intersect(paste(unique(strsplit(vo,".o",fixed=TRUE))),vi)  
    if (length(both) == 0) stop("no variables with observed and imputed values")
    vo <- paste(both,"o",sep=".")
-   diff <- (impObj[,both]-impObj[,vo,drop=FALSE])/
-             unlist(lapply(impObj[,vo,drop=FALSE],sd))
-   rmsd <- sqrt(apply(diff * diff,1,mean))
+   var <- unlist(lapply(impObj[,vo,drop=FALSE],var)) 
+   names(var) = sub(".o","",names(var),fixed = TRUE)  
+   diff <- impObj[,both,drop=FALSE]-impObj[,vo,drop=FALSE]
+   for (nam in both) diff[,nam] = (diff[,nam]*diff[,nam])/var[nam]
+   rmsd <- sqrt(apply(diff,1,mean))
    if (is.null(threshold)) threshold <- quantile(rmsd[refIds],1-p)
-   ans <- list(notablyDifferent.refs=sort(rmsd[refIds][rmsd[refIds]>threshold]),
+   ans <- list(call=cl,vars=both,threshold=threshold,
+               notablyDifferent.refs=sort(rmsd[refIds][rmsd[refIds]>threshold]),
                notablyDifferent.trgs=sort(rmsd[trgIds][rmsd[trgIds]>threshold]),
-               threshold=threshold,vars=both,rmsdS.refs=sort(rmsd[refIds]),
+               rmsdS.refs=sort(rmsd[refIds]),
                rmsdS.trgs=sort(rmsd[trgIds]))
    class(ans) <- "notablyDifferent"
    ans
@@ -61,12 +66,15 @@ plot.notablyDifferent <- function (x,add=FALSE,...)
     for (i in 1:length(x))
     {
       plot.notablyDifferent(x[[i]],xlim=xlim,ylim=ylim,add=i>1,col=i,...)
+      myusr = par()$usr
+      par(usr=c(0,1,0,1))
       cy <- par()$cxy[2]
       cy <- cy*1.1
-      if (i == 1) text(x=.05*xlim[2],y=.99*ylim[2],pos=4,"Legend")
-      text(x=.05*xlim[2],y=(.99*ylim[2])-(i+2)*cy,pos=4,
+      if (i == 1) text(x=.05,y=.95,pos=4,"Legend")
+      text(x=.05,y=.95-(i+2)*cy,pos=4,
           if (is.null(names(x)[i])) paste("Case",i) else
                       names(x)[i], col=i)
+      par(usr=myusr)
     }
   }
   else
@@ -87,15 +95,17 @@ plot.notablyDifferent <- function (x,add=FALSE,...)
     abline(h=x$threshold,...)
     if (!add)
     {
-      cxy = par()$cxy
-      cxy = cxy*1.1
-      bxy = c(.05*length(all),.99*max(all))
+      myusr = par()$usr
+      par(usr=c(0,1,0,1))
+      cxy = par()$cxy*1.1
+      bxy = c(.05,.95)
       points(x=cxy[1]  +bxy[1], y=bxy[2]-  cxy[2], pch=1)
       points(x=cxy[1]*2+bxy[1], y=bxy[2]-  cxy[2], pch=19)
       text(  x=cxy[1]*3+bxy[1], y=bxy[2]-  cxy[2], pos=4,"References")
       points(x=cxy[1]  +bxy[1], y=bxy[2]-2*cxy[2], pch=2)
       points(x=cxy[1]*2+bxy[1], y=bxy[2]-2*cxy[2], pch=17)
       text(  x=cxy[1]*3+bxy[1], y=bxy[2]-2*cxy[2], pos=4,"Targets")
+      par(usr=myusr)
     }
   }
 }
