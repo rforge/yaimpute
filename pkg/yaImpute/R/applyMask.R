37d8032c971e2eef2b52a15c04809c2dc6d36306
ncrookston <ncrookston@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-08-28 17:29:21 +0000

    An even faster version of applyMask and more edits to help files.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/yaimpute@32 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/yaImpute/R/applyMask.R
+++ pkg/yaImpute/R/applyMask.R
@@ -2,44 +2,50 @@ applyMask <- function
 (object, refGroups=NULL, trgGroups=NULL, method="removeWhenCommon", k=1)
 {
   if (class(object) != "yai") stop("object must be of class yai")
-  valid=c("removeWhenCommon","keepWhenCommon")
-  if (is.na(match(method,valid))) stop (paste("method must be one of",paste(valid,collapse=", ")))
+  valid <- c("removeWhenCommon","keepWhenCommon")
+  if (is.na(match(method,valid))) stop (paste("method must be one of",paste(valid,collapse<-", ")))
   if (is.null(refGroups) | is.null(trgGroups)) stop("refGroups and trgGroups must be defined")
   if (k >= object$k) stop("new value of k (",k,") must be less than old value (",object$k,")")
 
+  object$call <- match.call()
   refGrp <- refGroups[match(object$neiIdsTrgs,rownames(object$xRefs))]
-  lrefGrp <- if (method=="removeWhenCommon") refGrp != trgGroups else refGrp == trgGroups
+  lrefGrp <- if (method == "removeWhenCommon") refGrp != trgGroups else refGrp == trgGroups
   dim(lrefGrp) <- dim(object$neiIdsTrgs) 
 
-  # tvec is an offset in the storage of neiIdsTrgs and neiDstTrgs. 
-  tvec=0:(ncol(lrefGrp)-1) * nrow(lrefGrp)
+  # tvec is an offset in the storage of neiIdsTrgs and neiDstTrgs. At this point
+  # The kth member is the offset of the first row of the kth column
+  tvec <- 0:(ncol(lrefGrp)-1) * nrow(lrefGrp)
+  
+  # ans is the value of tvec corresponding the the columns to keep for each row.
   ans <- apply(lrefGrp,1,function(x,tvec,k) tvec[x][1:k],tvec,k)
-  dim (ans) = c(k,nrow(lrefGrp))
- 
-  tIds = vector(mode = "character", length = nrow(lrefGrp)*k)
-  tDst = vector(mode = "numeric",   length = nrow(lrefGrp)*k)
-  dim (tIds) = c(nrow(lrefGrp),k)
-  dim (tDst) = c(nrow(lrefGrp),k)
-  rownames(tIds) = rownames(object$neiIdsTrgs) 
-  rownames(tDst) = rownames(object$neiDstTrgs) 
-  colnames(tIds) = colnames(object$neiIdsTrgs)[1:k]
-  colnames(tDst) = colnames(object$neiDstTrgs)[1:k]
-
-  idx = 1:nrow(lrefGrp)
-  for (i in 1:k)
+  
+  # if k>1, we need to reorganize ans and delete the dimensions so it is a vector.
+  if (k>1) 
   {
-    jdx = idx+ans[i,]
-    tIds[,i] = object$neiIdsTrgs[jdx]
-    tDst[,i] = object$neiDstTrgs[jdx]
+    ans <- t(ans) 
+    dim(ans) <- NULL
   }
+
+  # now add the row numbers to ans...to get the final offsets. 
+  ans <- rep(1:nrow(lrefGrp),k) + ans
+  
+  rnB <- rownames(object$neiIdsTrgs)
+  cnI <- colnames(object$neiIdsTrgs)[1:k]
+  cnD <- colnames(object$neiDstTrgs)[1:k]
+  object$neiIdsTrgs <- object$neiIdsTrg[ans]
+  object$neiDstTrgs <- object$neiDstTrg[ans]
+  dim (object$neiIdsTrgs) <- c(nrow(lrefGrp),k)
+  dim (object$neiDstTrgs) <- c(nrow(lrefGrp),k)
+  rownames(object$neiIdsTrgs) <- rnB
+  rownames(object$neiDstTrgs) <- rnB 
+  colnames(object$neiIdsTrgs) <- cnI
+  colnames(object$neiDstTrgs) <- cnD
+
+  object$k <- k
+  object$noRefs <- TRUE
+  object$neiIdsRefs <- NULL
+  object$neiDstRefs <- NULL
  
-  object$call = match.call()
-  object$neiIdsTrgs = tIds
-  object$neiDstTrgs = tDst
-  object$k = k
-  object$noRefs = TRUE
-  object$neiIdsRefs = NULL
-  object$neiDstRefs = NULL
   object
 }
 
